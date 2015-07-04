20cb2918289dfed27e088efe1a89877e319b2255
tuliopires <tulio_po@hotmail.com>
2015-07-04 12:15:53 -0300

    Inclusado da biblioteca watches

--- Firmware/VE_1/src/VE_1.c
+++ Firmware/VE_1/src/VE_1.c
@@ -1,5 +1,7 @@
 #include "Board.h"
 #include "Threads.h"
+#include "watches.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 char fn1(void)
@@ -9,14 +11,38 @@ debug_s.data_tx[0] = 'T';
 debug_s.data_tx[1] = 'U';
 debug_s.data_tx[2] = 'L';
 	//debug_send_data_handler(3);
-	adc_read();
+	//adc_read();
 	LED_RGB_TGL(COLOUR_RED);
 
 	return 0;
 }
 char fn2(void)
 {
+	static char toggle =0;
 	LED_RGB_TGL(COLOUR_BLUE);
+	watches_run();
+	if(toggle== 0 )
+	{
+		watches_set_cronos(0);
+				toggle ++;
+	}
+	else
+	{
+		toggle ++;
+		if(toggle>2000)
+		{
+			uint32_t val;
+			unsigned char valor_str[7];
+			watches_set_cronos_flag(0,WATCH_CRONOS_FINISHED);
+			watches_get_cronos_counter(0,&val);
+			
+			sprintf(debug_s.data_tx ,"%0.5d" , val);
+			debug_s.data_tx [5] ='\r';
+			debug_s.data_tx [6] = '\n';
+			debug_send_data_handler(7);
+			toggle = 0;
+		}
+	}
 		
 	return 0;
 }
@@ -54,12 +80,11 @@ struct _motor_s
 void ve_1_init(void)
 {
 	board_init();
-	
-	
+	watches_init();
 	threads_init();
 	threads_constructor(&fn1,1000,0,0);
-	threads_constructor(&fn2,10000,0,1);
-	threads_constructor(&fn3,10000,0,2);
+	threads_constructor(&fn2,100,0,1);
+	threads_constructor(&fn3,100000,0,2);
 	
 	//configure_pwm();
 	//configure_ad();
