9923993e09d46d7ff6e8c49636805616338ef3ef
tuliopires <tulio_po@hotmail.com>
2015-07-04 12:16:32 -0300

    Criacao da biblioteca watches
    Foi implementado a primeira versao da biblioteca watches com funcao
    alarme e cronometro.

--- Firmware/VE_1/src/watches.c
+++ Firmware/VE_1/src/watches.c
@@ -4,3 +4,145 @@
  * Created: 7/1/2015 9:45:35 AM
  *  Author: Tulio
  */ 
+#include "watches.h"
+void watches_init()
+{
+	for(int i=0;i<WATCH_CRONOS_N;i++)
+	{
+		watch_crono_s[i].FLAG = WATCH_CRONOS_IDLE;
+	}
+	for(int i=0;i<WATCH_ALARM_N;i++)
+	{
+		watch_alarm_s[i].FLAG = WATCH_ALARM_IDLE;
+	}
+}
+char watches_set_cronos(int n) 
+{
+	if(n<WATCH_CRONOS_N)
+	{
+		if(watch_crono_s[n].FLAG == WATCH_CRONOS_COUNTING)
+		{
+			return -1;
+		}
+		
+		watch_crono_s[n].counter =0;
+		watch_crono_s[n].FLAG = WATCH_CRONOS_COUNTING;
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+char watches_get_cronos_ms(int n, uint16_t *val)
+{
+	uint32_t temp_val=0;
+	if(watches_get_cronos_counter(n, temp_val))
+	{
+		*val = temp_val/WATCH_FINAL_FREQ_KHZ;
+		return 0 ;
+	}
+	else
+	return -1;
+}
+char watches_get_cronos_counter(int n, uint32_t *val)
+{
+	if(n<WATCH_CRONOS_N)
+	{
+		if(watch_crono_s[n].FLAG == WATCH_CRONOS_COUNTING)
+		{
+			*val =0;
+			return -1;
+		}
+	
+		*val = watch_crono_s[n].counter;
+		return 1;
+	}
+	else
+	{
+		*val =0;
+		return -1;
+	}
+}
+char watches_set_cronos_flag(int n, int flag)
+{
+	if(n<WATCH_CRONOS_N)
+	{
+		watch_crono_s[n].FLAG = flag;
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+char watches_set_alarm_counter(int n, uint32_t referencia)
+{
+	if(n<WATCH_ALARM_N)
+{
+	if(watch_alarm_s[n].FLAG == WATCH_ALARM_COUNTING)
+	{
+		return -1;
+	}
+	watch_alarm_s[n].referencia = referencia;
+	watch_alarm_s[n].FLAG = WATCH_ALARM_RESETED;
+	return 1;
+}
+else
+	{
+		return -1;
+	}
+}
+char watches_get_alarm_flag(int n, int *flag)
+{
+	if(n<WATCH_CRONOS_N)
+	{
+		*flag = watch_alarm_s[n].FLAG ;
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+char watches_set_alarm_flag(int n, int flag)
+{
+	if(n<WATCH_CRONOS_N)
+	{
+		watch_alarm_s[n].FLAG =flag ;
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+char watches_run()
+{
+	for(int i=0;i<WATCH_CRONOS_N;i++)
+	{
+		
+		if(watch_crono_s[i].FLAG == WATCH_CRONOS_COUNTING)
+		{
+			watch_crono_s[i].counter++;
+		}
+		
+	}
+	for(int i=0;i<WATCH_ALARM_N;i++)
+	{
+		
+		if(watch_alarm_s[i].FLAG == WATCH_ALARM_COUNTING)
+		{
+			if(watch_alarm_s[i].counter == watch_alarm_s[i].referencia)
+			{
+				watch_alarm_s[i].FLAG == WATCH_ALARM_FINISHED;
+			}
+			else
+			{
+				watch_alarm_s[i].counter++;
+			}
+		}
+		
+	}
+}
--- Firmware/VE_1/src/watches.h
+++ Firmware/VE_1/src/watches.h
@@ -14,22 +14,42 @@
 /**
 	@var counter_current Contador contendo o valor atual do 
 */
-enum CRONOS_FLAG {CRONOS_IDLE, CRONOS_COUNTING, CRONOS_PAUSED, CRONOS_FINISHED, CRONOS_RESETED};
-enum CRONOS_FLAG {ALARM_IDLE, ALARM_COUNTING, ALARM_PAUSED, ALARM_FINISHED, ALARM_RESETED};
-	
+typedef enum WATCH_CRONOS_FLAG {WATCH_CRONOS_IDLE, WATCH_CRONOS_COUNTING, WATCH_CRONOS_PAUSED, WATCH_CRONOS_FINISHED, WATCH_CRONOS_RESETED};
+typedef enum WATCH_ALARM_FLAG {WATCH_ALARM_IDLE, WATCH_ALARM_COUNTING, WATCH_ALARM_PAUSED, WATCH_ALARM_FINISHED, WATCH_ALARM_RESETED};
+#define WATCH_CRONOS_N	3	
+#define WATCH_ALARM_N	3
+#define WATCH_CLOCK_SRC_FREQ	100000
+#define WATCH_THREAD_PRESCALER	10
+#define WATCH_FINAL_FREQ_HZ		WATCH_CLOCK_SRC_FREQ/WATCH_THREAD_PRESCALER
+#define WATCH_FINAL_FREQ_KHZ	WATCH_CLOCK_SRC_FREQ/(WATCH_THREAD_PRESCALER *1000)
+/**
+*	Struct contendo informacoes necessarias para gestao do cronometro.
+*/
 struct _watch_crono_s
 {
-	char FLAG;
-	unsigned long counter;
-	};
+	char FLAG; /**< Representa estado do cronometro */
+	uint32_t counter; /**< Valor incremental e instantaneo do cronometro.*/
+	}watch_crono_s[WATCH_CRONOS_N];
 
-
+/**
+*	Struct contendo informacoes necessarias para gestao do alarme 
+*/
 struct _watch_alarm_s
 {
-	char FLAG;
-	unsigned long counter;
-	
-};
+	char FLAG;/**< Representa estado do alarme */
+	uint32_t counter;/**< Valor incremental e instantaneo do alarme.*/
+	//unsigned long nova_referencia;	/**< Valor incremental e instantaneo do alarme.*/
+	uint32_t referencia;	/**< Valor de comparacao do alarme.*/
+}watch_alarm_s[WATCH_ALARM_N];
 
+void watches_init();
+char watches_set_cronos(int n);
+char watches_get_cronos_counter(int n, uint32_t *val);
+char watches_get_cronos_ms(int n, uint16_t *val);
+char watches_set_cronos_flag(int n, int flag);
+char watches_set_alarm_counter(int n, uint32_t referencia);
+char watches_get_alarm_flag(int n, int *flag);
+char watches_set_alarm_flag(int n, int flag);
+char watches_run();
 
 #endif /* WATCHES_H_ */
